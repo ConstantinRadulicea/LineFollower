00b498e33193cc1c3839e15ac497f59d644f51be
Constantin RADULICEA <constantinradulicea@gmail.com>
2024-10-09 20:14:19 +0300

    updates

--- src/TestLineFollower.cpp
+++ src/TestLineFollower.cpp
@@ -3,6 +3,7 @@
 #include <geometry2D.h>
 #include <SteeringController.h>
 #include "GlobalVariables.h"
+#include "PID.h"
 
 const float PID_Kp = 1.0f;
 
@@ -10,6 +11,7 @@ LineSensors lineSensors(TOTAL_LINE_SENSORS);
 float sensorsReadings[TOTAL_LINE_SENSORS];
 Point2D linePosition;
 SteeringController steeringController(1.0f, 0.0f, -1.0f);
+PID steering_pid(1.0, 0.0, 0.0, -1.0, 1.0, 0.0f);
 
 
 void setup()
@@ -34,9 +36,10 @@ void setup()
 float rotateTreshold = 0.5f;
 float maxSpeed = 0.25f;
 float speed = maxSpeed;
-float right_track_speed_cercentage = 1.0f;
-float left_track_speed_cercentage = 1.0f;
+float right_track_speed_percentage = 1.0f;
+float left_track_speed_percentage = 1.0f;
 float PID_out_right, PID_out_left;
+float steering_correction_pid_out;
 Point2D middleLineMax, middleLineMin;
 float blackLinePositionX, blackLinePositionY;
 
@@ -58,52 +61,60 @@ void loop()
   Serial.print("Min Posy:" + String(middleLineMin.y));
   Serial.print('\t');
 
-/*
-Pos_x:   Left: -1    Right: +1
-*/
+  /*
+  Pos_x:   Left: -1    Right: +1
+  */
+
+  /*
+  Black line is on the right sensor
+  Go full right
+  Stop right wheel and accelerate left wheel
+
+  left_wheel = 1;
+  right_wheel = 0;
+
+  blackLinePositionX = 1;
+  steering_correction_pid_out = -1;
+
+  left_wheel = 1 - steering_correction_pid_out; =  1 -(-1) = 2
+  right_wheel = 1 + steering_correction_pid_out; = 1 +(-1) = 0
 
 
-  if (blackLinePositionX < 0.0f) {
-    PID_out_right = 1.0f;
-    if (blackLinePositionX <= (-rotateTreshold)) {
-      PID_out_left = (blackLinePositionX + rotateTreshold) * 2.0f;
-    }
-    else{
-      PID_out_left = ((rotateTreshold) + blackLinePositionX) * 2.0f;
-    }
-  }
-  else{
-    PID_out_left = 1.0f;
-    if (blackLinePositionX <= (rotateTreshold)) {
-      PID_out_right = (rotateTreshold - blackLinePositionX) * 2.0f;
-    }
-    else{
-      PID_out_right = ((-blackLinePositionX) + rotateTreshold) * 2.0f;
-    }
-  }
-  
-  PID_out_right = PID_out_right * PID_Kp;
-  PID_out_left = PID_out_left * PID_Kp;
 
-  right_track_speed_cercentage = PID_out_right;
-  left_track_speed_cercentage = PID_out_left;
+  Black line is on the left sensor
+  Go full left
+  Accelerate right wheel and stop left wheel
+
+  left_wheel = 0;
+  right_wheel = 1;
+
+  blackLinePositionX = -1;
+  steering_correction_pid_out = 1;
+
+  left_wheel = 1 - steering_correction_pid_out; =  1 -(1) = 0
+  right_wheel = 1 + steering_correction_pid_out; = 1 +(1) = 2
+  */
+ 
+  steering_correction_pid_out = steering_pid.calculate(0, blackLinePositionX, 1.0);
+
+  left_track_speed_percentage = 1 - steering_correction_pid_out;
+  right_track_speed_percentage = 1 + steering_correction_pid_out;
 
 
-  left_track_speed_cercentage = MIN(left_track_speed_cercentage, 1.0f);
-  left_track_speed_cercentage = MAX(left_track_speed_cercentage, -1.0f);
-  right_track_speed_cercentage = MIN(right_track_speed_cercentage, 1.0f);
-  right_track_speed_cercentage = MAX(right_track_speed_cercentage, -1.0f);
+  left_track_speed_percentage = MIN(left_track_speed_percentage, 1.0f);
+  left_track_speed_percentage = MAX(left_track_speed_percentage, -1.0f);
+  right_track_speed_percentage = MIN(right_track_speed_percentage, 1.0f);
+  right_track_speed_percentage = MAX(right_track_speed_percentage, -1.0f);
 
 
-  Serial.print("left_track:" + String(left_track_speed_cercentage));
+  Serial.print("left_track:" + String(left_track_speed_percentage));
   Serial.print('\t');
-  Serial.print("right_track:" + String(right_track_speed_cercentage));
-
+  Serial.print("right_track:" + String(right_track_speed_percentage));
   Serial.println();
 
-  //toArduino = String(speed) + ";"+ String(left_track_speed_cercentage) + ";" + String(right_track_speed_cercentage);
+  //toArduino = String(speed) + ";"+ String(left_track_speed_percentage) + ";" + String(right_track_speed_percentage);
   //serial_soft1.print(toArduino);
-  steeringController.write(speed, left_track_speed_cercentage, right_track_speed_cercentage);
+  steeringController.write(speed, left_track_speed_percentage, right_track_speed_percentage);
   delay(10);
   //steeringController.write(1.0f, 1.0f, 1.0f);
 }
